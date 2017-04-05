0be77e9b8f663d5af4079445631dd211ae7655c0
Jose Caballero <jcaballero@bnl.gov>
2017-04-05 11:44:09 -0400

    dealing with version numbers

--- pluginmanager/__init__.py
+++ pluginmanager/__init__.py
@@ -5,15 +5,16 @@ Example:
 
 To get an instance of a plugin class "myplugin" in module
         package/plugins/typeA/kindB/myplugin.py
-that expect and integer as input to the __init__() method:
+that expects and integer as input to the __init__() method:
  
 >>> from pluginmanager import PluginManager
 >>> pm = PluginManager()
 >>> pluginobj = pm.getplugin(['package', 'plugins', 'typeA', 'kindB'], 'myplugin', 3)
 """
 
-from pluginmanager import PluginManager
+__version__ = '1.0.0'
 
+from pluginmanager import PluginManager
 
 __all__ = ['PluginManager',
           ]
--- setup.py
+++ setup.py
@@ -13,11 +13,13 @@ from distutils.core import setup
 from distutils.command.install import install as install_org
 from distutils.command.install_data import install_data as install_data_org
 
+import pluginmanager
+release_version = pluginmanager.__version__
 
 # setup for distutils
 setup(
     name="plugin-manager",
-    version="1",
+    version=release_version,
     description='plugin-manager package',
     long_description='''This package contains the plugins management''',
     license='GPL',
