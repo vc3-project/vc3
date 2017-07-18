18478a1b3fdb0962c03e16ebf3969d2c0f66bf67
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2017-07-18 16:13:10 -0500

    added credible /etc files

--- setup.py
+++ setup.py
@@ -8,6 +8,44 @@ from distutils.core import setup
 from credible import core
 release_version=core.__version__
 
+# ===========================================================
+#                D A T A     F I L E S
+# ===========================================================
+
+etc_files = [ 'etc/credible.conf',
+              'etc/openssl.cnf.intermediate.template',
+              'etc/openssl.cnf.root.template',
+            ]
+
+home_data_files = [
+            ('etc', etc_files)
+            ]
+
+rpm_data_files= [
+            ('/etc/credible', etc_files)
+            ]
+
+def choose_data_files():
+    rpminstall = True
+    userinstall = False
+
+    if 'bdist_rpm' in sys.argv:
+        rpminstall = True
+
+    elif 'install' in sys.argv:
+        for a in sys.argv:
+            if a.lower().startswith('--home'):
+                rpminstall = False
+                userinstall = True
+
+    if rpminstall:
+        return rpm_data_files
+    elif userinstall:
+        return home_data_files
+    else:
+        # Something probably went wrong, so punt
+        return rpm_data_files
+
 setup(
     name="credible",
     version=release_version,
@@ -30,8 +68,6 @@ setup(
           'Topic :: System Administration :: Management',
     ],
     scripts=[ 'scripts/credible',
-             ]
-
+             ],
+    data_files = choose_data_files()
 )
-
-
